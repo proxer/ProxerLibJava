d83ce7d40454db92ce398ee853849886bd4276bf
Ruben Gees <geesruben@yahoo.de>
2015-10-17 14:43:50 +0200

    Add ProxerInfo class

---
+++ library/src/main/java/com/proxerme/library/util/ProxerInfo.java
@@ -0,0 +1,13 @@
+package com.proxerme.library.util;
+
+/**
+ * Todo: Describe Class
+ *
+ * @author Ruben Gees
+ */
+public class ProxerInfo {
+
+    public static final int NEWS_ON_PAGE = 15;
+    public static final int CONFERENCES_ON_PAGE = 48;
+
+}
