da7f5b5f40f385d37c3966e690561aaebd1f4c49
Ruben Gees <geesruben@yahoo.de>
2016-03-31 20:29:33 +0200

    Also implement Parcelable, equals and hashcode

--- library/src/main/java/com/proxerme/library/entity/Message.java
+++ library/src/main/java/com/proxerme/library/entity/Message.java
@@ -1,5 +1,7 @@
 package com.proxerme.library.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
@@ -12,15 +14,24 @@ import com.proxerme.library.interfaces.TimeItem;
  *
  * @author Ruben Gees
  */
-public class Message implements IdItem, ImageItem, TimeItem {
+public class Message implements IdItem, ImageItem, TimeItem, Parcelable {
 
+    public static final Parcelable.Creator<Message> CREATOR = new Parcelable.Creator<Message>() {
+        @Override
+        public Message createFromParcel(Parcel source) {
+            return new Message(source);
+        }
+
+        @Override
+        public Message[] newArray(int size) {
+            return new Message[size];
+        }
+    };
     private String id;
     private String fromId;
     private String message;
-
     @Nullable
     private Integer action;
-
     private long time;
     private String device;
     private String username;
@@ -37,6 +48,17 @@ public class Message implements IdItem, ImageItem, TimeItem {
         this.device = device;
         this.username = username;
         this.imageId = imageId;
+    }
+
+    protected Message(Parcel in) {
+        this.id = in.readString();
+        this.fromId = in.readString();
+        this.message = in.readString();
+        this.action = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.time = in.readLong();
+        this.device = in.readString();
+        this.username = in.readString();
+        this.imageId = in.readString();
     }
 
     @NonNull
@@ -92,6 +114,56 @@ public class Message implements IdItem, ImageItem, TimeItem {
     @Override
     public String getImageId() {
         return imageId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Message message1 = (Message) o;
+
+        if (time != message1.time) return false;
+        if (!id.equals(message1.id)) return false;
+        if (!fromId.equals(message1.fromId)) return false;
+        if (!message.equals(message1.message)) return false;
+        if (action != null ? !action.equals(message1.action) : message1.action != null)
+            return false;
+        if (device != null ? !device.equals(message1.device) : message1.device != null)
+            return false;
+        if (!username.equals(message1.username)) return false;
+        return imageId.equals(message1.imageId);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + fromId.hashCode();
+        result = 31 * result + message.hashCode();
+        result = 31 * result + (action != null ? action.hashCode() : 0);
+        result = 31 * result + (int) (time ^ (time >>> 32));
+        result = 31 * result + (device != null ? device.hashCode() : 0);
+        result = 31 * result + username.hashCode();
+        result = 31 * result + imageId.hashCode();
+        return result;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.fromId);
+        dest.writeString(this.message);
+        dest.writeValue(this.action);
+        dest.writeLong(this.time);
+        dest.writeString(this.device);
+        dest.writeString(this.username);
+        dest.writeString(this.imageId);
     }
 
     public static class ConferenceAction {
