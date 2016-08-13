796952784754cc342bc7ce7e597f6ba9f321ff9d
Ruben Gees <geesruben@yahoo.de>
2016-08-13 21:11:38 +0200

    Cleanup constants: Remove unused ones and prefix each with its class

--- library/src/main/java/com/proxerme/library/info/ProxerTag.java
+++ library/src/main/java/com/proxerme/library/info/ProxerTag.java
@@ -13,36 +13,34 @@ import java.lang.annotation.Target;
 public class ProxerTag {
 
     public static final int NEWS = 0;
-    public static final int LOGIN = 1;
-    public static final int LOGOUT = 2;
-    public static final int USERINFO = 3;
-    public static final int TOPTEN = 4;
-    public static final int USER_MEDIA_LIST = 5;
-    public static final int MEDIA_LIST = 6;
-    public static final int MEDIA_SEARCH = 7;
 
-    public static final int INFO_ENTRY_CORE = 20;
-    public static final int INFO_ENTRY_SYNONYM = 21;
-    public static final int INFO_ENTRY_SEASON = 22;
+    public static final int LOGIN = 10;
+    public static final int LOGOUT = 11;
 
-    public static final int MESSENGER_CONFERENCES = 30;
-    public static final int MESSENGER_MESSAGES = 31;
-    public static final int MESSENGER_SEND_MESSAGE = 32;
-    public static final int MESSENGER_CONFERENCE_CONSTANTS = 33;
-    public static final int MESSENGER_CONFERENCE_INFO = 34;
+    public static final int USER_INFO = 20;
+    public static final int USER_TOPTEN = 21;
+    public static final int USER_MEDIA_LIST = 22;
 
-    public static final int CONFERENCES = 100;
-    public static final int CHAT = 101;
-    public static final int SEND_MESSAGE = 102;
+    public static final int MEDIA_LIST = 30;
+    public static final int MEDIA_SEARCH = 31;
+
+    public static final int INFO_ENTRY_CORE = 40;
+    public static final int INFO_ENTRY_SYNONYM = 41;
+    public static final int INFO_ENTRY_SEASON = 42;
+
+    public static final int MESSENGER_CONFERENCES = 50;
+    public static final int MESSENGER_MESSAGES = 51;
+    public static final int MESSENGER_SEND_MESSAGE = 52;
+    public static final int MESSENGER_CONFERENCE_CONSTANTS = 53;
+    public static final int MESSENGER_CONFERENCE_INFO = 54;
 
     /**
      * An annotation representing all the different tags.
      */
-    @IntDef({LOGIN, NEWS, LOGOUT, USERINFO, TOPTEN, CONFERENCES, CHAT, SEND_MESSAGE,
-            USER_MEDIA_LIST, MEDIA_LIST, MEDIA_SEARCH,
-            INFO_ENTRY_CORE, INFO_ENTRY_SYNONYM, INFO_ENTRY_SEASON,
-            MESSENGER_CONFERENCES, MESSENGER_MESSAGES, MESSENGER_SEND_MESSAGE,
-            MESSENGER_CONFERENCE_CONSTANTS, MESSENGER_CONFERENCE_INFO})
+    @IntDef({LOGIN, NEWS, LOGOUT, USER_INFO, USER_TOPTEN, USER_MEDIA_LIST, MEDIA_LIST, MEDIA_SEARCH,
+            INFO_ENTRY_CORE, INFO_ENTRY_SYNONYM, INFO_ENTRY_SEASON, MESSENGER_CONFERENCES,
+            MESSENGER_MESSAGES, MESSENGER_SEND_MESSAGE, MESSENGER_CONFERENCE_CONSTANTS,
+            MESSENGER_CONFERENCE_INFO})
     @Retention(RetentionPolicy.SOURCE)
     @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
     public @interface ConnectionTag {
