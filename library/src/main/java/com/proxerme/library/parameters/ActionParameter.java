2134f957309967bcbf9c843c8d8da68177578a6d
Ruben Gees <geesruben@yahoo.de>
2016-08-17 20:55:27 +0200

    Update support library

---
+++ library/src/main/java/com/proxerme/library/parameters/ActionParameter.java
@@ -0,0 +1,14 @@
+package com.proxerme.library.parameters;
+
+/**
+ * TODO: Describe class
+ *
+ * @author Ruben Gees
+ */
+
+public class ActionParameter {
+
+    public static final String ADD_USER = "addUser";
+    public static final String REMOVE_USER = "removeUser";
+
+}
