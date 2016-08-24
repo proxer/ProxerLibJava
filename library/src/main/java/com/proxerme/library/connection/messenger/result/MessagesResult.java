baba2c476162be4a183bd574d750ddcc87d16a76
Ruben Gees <geesruben@yahoo.de>
2016-08-24 03:42:01 +0200

    Fix MessagesRequest

--- library/src/main/java/com/proxerme/library/connection/messenger/result/MessagesResult.java
+++ library/src/main/java/com/proxerme/library/connection/messenger/result/MessagesResult.java
@@ -2,9 +2,11 @@ package com.proxerme.library.connection.messenger.result;
 
 import com.proxerme.library.connection.ProxerResult;
 import com.proxerme.library.connection.messenger.entity.Message;
+import com.squareup.moshi.Json;
 
 public final class MessagesResult extends ProxerResult<Message[]> {
 
+    @Json(name = "data")
     private Message[] messages;
 
     private MessagesResult() {
