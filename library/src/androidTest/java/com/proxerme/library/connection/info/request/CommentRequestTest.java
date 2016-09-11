5c349d50ccc8a71b17c31a7d8f7f8362d40c5f63
Desnoo <TinoSiegmund@gmx.de>
2016-09-11 23:47:09 +0200

    Fix of CommentsRequest Test.

--- library/src/androidTest/java/com/proxerme/library/connection/info/request/CommentRequestTest.java
+++ library/src/androidTest/java/com/proxerme/library/connection/info/request/CommentRequestTest.java
@@ -4,9 +4,9 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.proxerme.library.connection.ProxerException;
 import com.proxerme.library.connection.info.entity.Comment;
+import com.proxerme.library.connection.info.entity.RatingDetails;
 import com.proxerme.library.parameters.CommentSortParameter;
 import com.proxerme.library.parameters.CommentStateParameter;
-import com.proxerme.library.connection.info.entity.RatingDetails;
 import com.proxerme.library.test.R;
 import com.proxerme.library.util.RequestTest;
 
@@ -39,9 +39,8 @@ public class CommentRequestTest extends RequestTest {
         server.enqueue(new MockResponse().setBody(loadResponse(R.raw.comment_anime)));
 
         Comment[] comments = connection.executeSynchronized(new CommentRequest(ID)
-                .withCustomHost(buildHostUrl(server.url(URL))));
-        Comment[] comments = connection.executeSynchronized(new CommentRequest(PAGE)
                                                                     .withCustomHost(buildHostUrl(server.url(URL))));
+
         assertEquals(25, comments[0].getEpisode());
         RatingDetails details = comments[0].getRatingDetails();
         assertEquals(3, details.getGenre());
@@ -58,7 +57,7 @@ public class CommentRequestTest extends RequestTest {
         server.enqueue(new MockResponse().setBody(loadResponse(R.raw.comment_anime)));
 
         Comment[] comments = connection.executeSynchronized(new CommentRequest(ID)
-                .withCustomHost(buildHostUrl(server.url(URL))));
+                                                                    .withCustomHost(buildHostUrl(server.url(URL))));
 
         assertEquals(25, comments.length);
     }
@@ -68,7 +67,7 @@ public class CommentRequestTest extends RequestTest {
         server.enqueue(new MockResponse().setBody(loadResponse(R.raw.comment_anime)));
 
         connection.executeSynchronized(new CommentRequest(ID)
-                .withCustomHost(buildHostUrl(server.url(URL))));
+                                               .withCustomHost(buildHostUrl(server.url(URL))));
 
         assertEquals(URL, server.takeRequest().getPath());
     }
@@ -78,9 +77,8 @@ public class CommentRequestTest extends RequestTest {
         server.enqueue(new MockResponse().setBody(loadResponse(R.raw.comment_manga)));
 
         Comment[] comments = connection.executeSynchronized(new CommentRequest(ID)
-                .withCustomHost(buildHostUrl(server.url(URL))));
-        Comment[] comments = connection.executeSynchronized(new CommentRequest(PAGE)
                                                                     .withCustomHost(buildHostUrl(server.url(URL))));
+
         assertEquals(63, comments[0].getEpisode());
         RatingDetails details = comments[0].getRatingDetails();
         assertEquals(5, details.getGenre());
@@ -96,7 +94,7 @@ public class CommentRequestTest extends RequestTest {
         server.enqueue(new MockResponse().setBody(loadResponse(R.raw.comment_manga)));
 
         Comment[] comments = connection.executeSynchronized(new CommentRequest(ID)
-                .withCustomHost(buildHostUrl(server.url(URL))));
+                                                                    .withCustomHost(buildHostUrl(server.url(URL))));
 
         assertEquals(1, comments.length);
     }
@@ -106,38 +104,40 @@ public class CommentRequestTest extends RequestTest {
         server.enqueue(new MockResponse().setBody(loadResponse(R.raw.comment_anime)));
 
         connection.executeSynchronized(new CommentRequest(ID).withPage(0).withLimit(10)
-                .withSortType(CommentSortParameter.RATING)
-                .withCustomHost(buildHostUrl(server.url(PARAMETER_URL))));
+                                               .withSortType(CommentSortParameter.RATING)
+                                               .withCustomHost(buildHostUrl(server.url(PARAMETER_URL))));
 
         assertEquals(PARAMETER_URL, server.takeRequest().getPath());
     }
 
     private Comment generateTestComment() {
         return new Comment("17835118", "4167", "583403", "entry", CommentStateParameter.WATCHED,
-                "Dieser hervorragende Anime hat mich schon am Anfang gefesselt und, am Ende auch " +
-                        "nicht enteuscht. Die gut in Szene gesetzte und abwechslungsreiche Story " +
-                        "wird je nach Situation, sowol von friedlicher als auch von " +
-                        "actionreicher Musik begleitet. Weiterhin sind die Animationen gut " +
-                        "gelungen, und die Charaktere ziehen einen mit ihrem Charm in ihren " +
-                        "Bann. Daher sollte jeder der auf das Genre steht und, oder sich einen " +
-                        "Otaku nennt diesen Anime gesehen haben.", 8, 25, 0, 1472063559, "Lorak",
-                "583403_pSCA43.jpg");
+                           new RatingDetails(3, 4, 3, 5, 4),
+                           "Dieser hervorragende Anime hat mich schon am Anfang gefesselt und, am Ende auch " +
+                                   "nicht enteuscht. Die gut in Szene gesetzte und abwechslungsreiche Story " +
+                                   "wird je nach Situation, sowol von friedlicher als auch von " +
+                                   "actionreicher Musik begleitet. Weiterhin sind die Animationen gut " +
+                                   "gelungen, und die Charaktere ziehen einen mit ihrem Charm in ihren " +
+                                   "Bann. Daher sollte jeder der auf das Genre steht und, oder sich einen " +
+                                   "Otaku nennt diesen Anime gesehen haben.", 8, 25, 0, 1472063559, "Lorak",
+                           "583403_pSCA43.jpg");
     }
 
     private Comment generateTestMangaComment() {
         return new Comment("7502937", "6195", "293356", "entry", CommentStateParameter.WATCHING,
-                "Hallo zusammen,\n\nIch wollte nur kurz festhalten, dass dieser Manga einfach " +
-                        "- zumindest in meinen Augen - ein Meisterwerk ist! " +
-                        "\n\nGenres stimmen - 5/5\nStory ist herzerwärmend - " +
-                        "5/5\nZeichenstil ist genial - 5/5\nCharaktere sehr ausgeprägt, " +
-                        "unterschiedlich, erfrischend, aufeinander abgestimmt 6/5 (ich liebe " +
-                        "einfach jeden Charakter)\n\nEcht jetzt, mein Bauch schmerzt schon " +
-                        "fast vom vielen Lachen ^_^\nDieser Manga ist echt und wirklich " +
-                        "lesenswert. Er erzählt vom alltäglichen Leben von Handa Seishuu, " +
-                        "seinem unbewussten Reifen in der Kalligraphie und vielen kleine " +
-                        "Abenteuer, die einfach unbezahlbar und schön mitzuerleben sind " +
-                        "^_^\n\nMeine Empfehlung, einer meiner Top 10 Mangas " +
-                        "<3\n\nEure V.K.C.", 10, 63, 1, 1460844261, "V.K.C.",
-                "293356_5356c84799fed.jpg");
+                           new RatingDetails(5, 5, 4, 5, 0),
+                           "Hallo zusammen,\n\nIch wollte nur kurz festhalten, dass dieser Manga einfach " +
+                                   "- zumindest in meinen Augen - ein Meisterwerk ist! " +
+                                   "\n\nGenres stimmen - 5/5\nStory ist herzerwärmend - " +
+                                   "5/5\nZeichenstil ist genial - 5/5\nCharaktere sehr ausgeprägt, " +
+                                   "unterschiedlich, erfrischend, aufeinander abgestimmt 6/5 (ich liebe " +
+                                   "einfach jeden Charakter)\n\nEcht jetzt, mein Bauch schmerzt schon " +
+                                   "fast vom vielen Lachen ^_^\nDieser Manga ist echt und wirklich " +
+                                   "lesenswert. Er erzählt vom alltäglichen Leben von Handa Seishuu, " +
+                                   "seinem unbewussten Reifen in der Kalligraphie und vielen kleine " +
+                                   "Abenteuer, die einfach unbezahlbar und schön mitzuerleben sind " +
+                                   "^_^\n\nMeine Empfehlung, einer meiner Top 10 Mangas " +
+                                   "<3\n\nEure V.K.C.", 10, 63, 1, 1460844261, "V.K.C.",
+                           "293356_5356c84799fed.jpg");
     }
 }
