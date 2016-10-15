880f03117a4abc59d14bf8a7e08643da72839c17
Ruben Gees <geesruben@yahoo.de>
2016-10-15 18:25:33 +0200

    Implement SetReminderRequest

---
+++ library/src/main/java/com/proxerme/library/connection/ucp/result/SetReminderResult.java
@@ -0,0 +1,20 @@
+package com.proxerme.library.connection.ucp.result;
+
+import android.support.annotation.Nullable;
+
+import com.proxerme.library.connection.ProxerResult;
+
+/**
+ * {@inheritDoc}
+ */
+public class SetReminderResult extends ProxerResult<Void> {
+
+    protected SetReminderResult() {
+    }
+
+    @Override
+    @Nullable
+    public Void getData() {
+        return null;
+    }
+}
