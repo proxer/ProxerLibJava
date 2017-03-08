64ccf65ce46fcf54a7642a05459fc4c1cdf3c6fc
Ruben Gees <rubengees7@googlemail.com>
2017-03-08 17:42:51 +0100

    Make lombok generate proper JavaDoc and Sources

--- library/src/main/java/com/proxerme/library/entitiy/info/Comment.java
+++ library/src/main/java/com/proxerme/library/entitiy/info/Comment.java
@@ -16,43 +16,87 @@ import java.util.Date;
  *
  * @author Desnoo
  */
+@SuppressWarnings("JavaDoc")
 @Value
 public final class Comment implements IdItem, ImageItem, TimeItem {
 
     /**
-     * The id of this comment.
+     * @return The id of this comment.
      */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "id")
     private String id;
 
+    /**
+     * @return The id of the associated entry.
+     */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "tid")
     private String entryId;
+
+    /**
+     * @return The id of the author.
+     */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "uid")
     private String authorId;
+
+    /**
+     * @return The progress, the user has made on the associated media.
+     */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "state")
     private UserMediaProgress mediaProgress;
+
+    /**
+     * @return Finer grained ratings for e.g. music.
+     */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "data")
     private RatingDetails ratingDetails;
+
+    /**
+     * @return The actual content of the comment.
+     */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "comment")
     private String content;
+
+    /**
+     * @return The overall rating.
+     */
     @Json(name = "rating")
     private int overallRating;
+
+    /**
+     * @return The episode, the user is currently at.
+     */
     @Json(name = "episode")
     private int episode;
+
+    /**
+     * @return The amount of helpful votes by other users.
+     */
     @Json(name = "positive")
     private int helpfulVotes;
+
+    /**
+     * @return The time, the comment was updated the last time.
+     */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "timestamp")
     private Date time;
+
+    /**
+     * @return The username of the author.
+     */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "username")
     private String author;
+
+    /**
+     * @return The id of the author's image.
+     */
     @Getter(onMethod = @__(@NotNull))
     @Json(name = "avatar")
     private String imageId;
