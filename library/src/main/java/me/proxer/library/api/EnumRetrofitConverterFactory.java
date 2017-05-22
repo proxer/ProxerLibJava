12177fd7efb06f618a14b0716041de31d27a3f57
Ruben Gees <rubengees7@googlemail.com>
2017-05-23 01:44:50 +0200

    Require Java 8 and update dependencies
    This is possible as Android now supports Java 8.

--- library/src/main/java/me/proxer/library/api/EnumRetrofitConverterFactory.java
+++ library/src/main/java/me/proxer/library/api/EnumRetrofitConverterFactory.java
@@ -4,7 +4,6 @@ import me.proxer.library.util.ProxerUtils;
 import retrofit2.Converter;
 import retrofit2.Retrofit;
 
-import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 
@@ -17,16 +16,9 @@ final class EnumRetrofitConverterFactory extends Converter.Factory {
     public Converter<?, String> stringConverter(final Type type, final Annotation[] annotations,
                                                 final Retrofit retrofit) {
         if (((Class<?>) type).isEnum()) {
-            return new EnumConverter();
+            return (Converter<Enum<?>, String>) ProxerUtils::getApiEnumName;
         }
 
         return null;
-    }
-
-    private static final class EnumConverter implements Converter<Enum<?>, String> {
-        @Override
-        public String convert(final Enum<?> e) throws IOException {
-            return ProxerUtils.getApiEnumName(e);
-        }
     }
 }
