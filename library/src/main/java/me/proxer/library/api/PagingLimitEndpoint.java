2fa14abc639c24d34eca2b2c08afa0c26e6cb5aa
Ruben Gees <rubengees7@googlemail.com>
2017-07-28 00:05:09 +0200

    Make the PagingLimitEndpoint return itself correctly

--- library/src/main/java/me/proxer/library/api/PagingLimitEndpoint.java
+++ library/src/main/java/me/proxer/library/api/PagingLimitEndpoint.java
@@ -1,9 +1,20 @@
 package me.proxer.library.api;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 /**
  * Interface for classes which implement both {@link PagingEndpoint} and {@link LimitEndpoint}.
  *
  * @author Ruben Gees
  */
 public interface PagingLimitEndpoint<T> extends PagingEndpoint<T>, LimitEndpoint<T> {
+
+    @Override
+    @Nonnull
+    PagingLimitEndpoint<T> page(@Nullable Integer page);
+
+    @Override
+    @Nonnull
+    PagingLimitEndpoint<T> limit(@Nullable Integer limit);
 }
