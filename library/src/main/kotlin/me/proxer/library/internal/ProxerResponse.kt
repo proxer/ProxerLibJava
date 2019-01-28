36e11ba01dec195277a4b5b092c93f5767b3485c
Ruben Gees <rubengees7@googlemail.com>
2019-01-29 00:25:46 +0100

    Fix ProxerResponse not being a JsonClass

--- library/src/main/kotlin/me/proxer/library/internal/ProxerResponse.kt
+++ library/src/main/kotlin/me/proxer/library/internal/ProxerResponse.kt
@@ -1,17 +1,19 @@
 package me.proxer.library.internal
 
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
 import me.proxer.library.internal.adapter.NumberBasedBoolean
 
 /**
  * @author Ruben Gees
  */
+@JsonClass(generateAdapter = true)
 internal data class ProxerResponse<T>(
-    @field:NumberBasedBoolean @Json(name = "error") private val error: Boolean,
+    @field:NumberBasedBoolean @Json(name = "error") internal val error: Boolean,
     @Json(name = "message") val message: String,
-    @Json(name = "code") val code: Int,
+    @Json(name = "code") val code: Int?,
     @Json(name = "data") val data: T?
 ) {
 
-    val isSuccessful get() = !error
+    val isSuccessful = !error
 }
